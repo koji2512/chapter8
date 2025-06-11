e57266c2caf57511b9d7de00681fbb16400f6d3f
kojimahayato <241029@std.hi-joho.ac.jp>
2025-06-11 09:29:44 +0900

    あ

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-        // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.printf("Hello and welcome!");
+        Sword s = new Sword();
+        s.name = "炎の剣";
+        s.damage = 10;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-            // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
-        }
+        Hero h1 =new Hero("ミナト");
+        //h1.name = "ミナト";
+        System.out.println(h1.hp);
+        System.out.println(h1.name);
+        h1.sword = s;
+
+       /* Hero h2 = new Hero();
+        h2.name = "アサカ";
+        System.out.println(h2.hp);
+
+        Wizard w = new Wizard();
+        w.name = "スガワラ";
+       System.out.println(w.hp);*/
+
+        System.out.println("勇者"+h1.name+"を生み出しました！");
+        System.out.println("現在の武器は"+h1.sword.name);
+
+        Matango m1= new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
+
+
+        /*h.sit(5);
+        h.slip();
+        h.sit(25);
+        h.run();
+
+       h.slip();
+        m1.run();
+        m2.run();
+        h.run();
+
+       m1.attack();
+        h1.attack();
+        h1.slip();
+        h1.slip();
+        h1.sit(25);
+        m1.run();
+        h1.slip();
+        h1.sleep();
+        w.heal(h1);
+        w.heal(h2);
+        w.heal(h2);
+        /*m2.run();
+        h1.run();*/
+
+             /*   Hero h1;
+                h1 = new Hero();
+                h1.hp = 100;
+                Hero h2 = h1;
+                h2.hp = 200;
+                System.out.println(h1.hp);*/
     }
 }
